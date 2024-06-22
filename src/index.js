fc2f6232b8a2bde4e01dd92377aa778810233092
andrew-emil <147343944+andrew-emil@users.noreply.github.com>
2024-06-22 20:59:35 +0300

    project setup 2

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from './frontend/pages/HomePage';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HomePage/>)
+
